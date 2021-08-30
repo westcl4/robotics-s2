2ad5cec124b4863546b5c2750a487c8863d9a29c
Conor <westcl4@student.op.ac.nz>
2021-08-31 11:20:42 +1200

    Updated zumo line sensor file

--- samples/Zumo_Line_Sensor_Test/Zumo_Line_Sensor_Test.ino
+++ samples/Zumo_Line_Sensor_Test/Zumo_Line_Sensor_Test.ino
@@ -4,9 +4,16 @@
 //#include <Pushbutton.h>
 //#include <QTRSensors.h>
 //#include <ZumoBuzzer.h>
-//#include <ZumoMotors.h>
+#include <ZumoMotors.h>
 #include <ZumoReflectanceSensorArray.h>
 #include <ZumoShield.h>
+#include <Wire.h>
+LSM303 compass;
+L3G gyro;
+ZumoMotors motors;
+float old_tap_value = 0;
+float new_tap_value = 0;
+boolean tap_flag = false;
 
 ZumoReflectanceSensorArray reflectanceSensors;
 #define NUM_SENSORS 6  // number of sensors used
@@ -16,6 +23,14 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   reflectanceSensors.init();
+  Wire.begin();
+  compass.init();
+  compass.enableDefault();
+  delay(5000);
+  compass.read();
+  new_tap_value = compass.a.z;
+  gyro.init();
+  gyro.enableDefault();
 }
 
 void loop() {
@@ -31,4 +46,16 @@ void loop() {
   Serial.println("");
   
   delay(250);
+}
+
+void gyroread() {
+ 
+}
+
+void forward() {
+  
+}
+
+void linesensors() {
+  
 }
