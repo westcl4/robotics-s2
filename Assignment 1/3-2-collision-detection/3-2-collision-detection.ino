9f8cb87dae09c09647888ea6d7fb07387959f491
Conor <westcl4@student.op.ac.nz>
2021-08-31 20:48:54 +1200

    Added methods to assignment 3-1

--- Assignment 1/3-2-collision-detection/3-2-collision-detection.ino
+++ Assignment 1/3-2-collision-detection/3-2-collision-detection.ino
@@ -1,9 +1,66 @@
+#include <ZumoMotors.h>
+#include <ZumoShield.h>
+#include <Wire.h>
+
+LSM303 compass;
+L3G gyro;
+ZumoMotors motors;
+
+int botSpeed, stopTime, difference;
+float old_tap_value = 0;
+float new_tap_value = 0;
+boolean collision_detected = false;
+
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  Wire.begin();
+  compass.init();
+  compass.enableDefault();
+  compass.read();
+  delay(500);
+  new_tap_value = compass.a.z;
+  gyro.init();
+  gyro.enableDefault();
 }
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
+  compass.read();
+  gyro.read();
+
+  Serial.print(gyro.g.x);
+  Serial.print(gyro.g.y);
+  Serial.print(gyro.g.z);
+  Serial.println();
+
+  old_tap_value = new_tap_value;
+  new_tap_value = compass.a.z;
+
+  float diff = (new_tap_value - old_tap_value);
+  Serial.println(diff);
+
+  difference = diff;
+
+  if (difference >= 1500)
+  {
+    backup();
+    delay(10000);
+  }
+  drive_forward(100);
+}
+
+void drive_forward(int botSpeed) {
+  motors.setSpeeds(botSpeed, botSpeed);
+  //  delay(stopTime);
+  //  motors.setSpeeds(0, 0);
+  //  tap_flag = false;
+  //  return;
+}
+
+void backup() {
 
 }
