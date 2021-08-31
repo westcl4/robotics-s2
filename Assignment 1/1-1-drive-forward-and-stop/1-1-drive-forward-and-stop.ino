6d952c6d674e6d71cb4b7393b3e38f2920eb26de
Conor <westcl4@student.op.ac.nz>
2021-08-31 14:55:01 +1200

    Finished assignment 1.1

--- Assignment 1/1-1-drive-forward-and-stop/1-1-drive-forward-and-stop.ino
+++ Assignment 1/1-1-drive-forward-and-stop/1-1-drive-forward-and-stop.ino
@@ -4,13 +4,21 @@
 
 ZumoMotors motors;
 
+int speed = 100;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Wire.begin();
+
+  motors.setSpeeds (speed, speed);
+  delay(2000);
+  motors.setSpeeds (0,0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
+  
+  
 }
