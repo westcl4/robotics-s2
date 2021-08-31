5c7f22013c67bb26b2f5b31abf309bfb60060734
Conor <westcl4@student.op.ac.nz>
2021-08-31 16:45:58 +1200

    added initial files for task 3-1

--- Assignment 1/3-1-tap-start/3-1-tap-start.ino
+++ Assignment 1/3-1-tap-start/3-1-tap-start.ino
@@ -1,9 +1,12 @@
-#include <Wire.h>
+#include <ZumoMotors.h>
 #include <ZumoShield.h>
+#include <Wire.h>
 
+ZumoMotors motors;
 LSM303 compass;
 L3G gyro;
-ZumoMotors motors;
+
+int speed = 150;
 float old_tap_value = 0;
 float new_tap_value = 0;
 boolean tap_flag = false;
@@ -14,17 +17,21 @@ void setup() {
   Wire.begin();
   compass.init();
   compass.enableDefault();
-  delay(5000);
   compass.read();
+  delay(5000);
   new_tap_value = compass.a.z;
   gyro.init();
   gyro.enableDefault();
+
+
 }
 
-void loop() {
-  compass.read(); //both accelerometer and compass
 
-    gyro.read();
+void loop() {
+  // put your main code here, to run repeatedly:
+  compass.read();
+
+  gyro.read();
   Serial.print(gyro.g.x);
   Serial.print(gyro.g.y);
   Serial.print(gyro.g.z);
@@ -32,25 +39,22 @@ void loop() {
 
   old_tap_value = new_tap_value;
   new_tap_value = compass.a.z;
-  
+
   float diff = (new_tap_value - old_tap_value);
   Serial.println(diff);
-  
+
   if (diff > 1500)
   {
     tap_flag = true;
-    }
+  }
 
   if (tap_flag == true)
   {
     drive_forward(100);
-    
-    }
-  
+  }
 }
-
-void drive_forward(int speed){
+void drive_forward(int speed) {
   motors.setSpeeds(speed, speed);
   delay(1000);
-  motors.setSpeeds(0,0);
-  }
+  motors.setSpeeds(0, 0);
+}
